ecc727fa3925757e183d67ab078352bbc2659a7f
BIRB <tiagojralmeida04@outlook.com>
2024-02-16 20:17:20 +0000

    Display of numbers correction + addition of numbers array

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,4 +1,14 @@
-function test(number){
-    document.getElementById("result").innerTMLL = "number";
+const numbers = [];
+
+function writeNumber(number){
+    if(numbers.length <= 1){
+        document.getElementById("result").innerHTML = number;
+        numbers[0] = number;
+    }
+    else{
+        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML + number;
+        numbers[2] = number;
+    }
     console.log(number);
-}}
+    console.log(numbers);
+}
