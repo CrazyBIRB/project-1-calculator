1dda2be95de02206bbbdd72300158f11ab06482b
BIRB <tiagojralmeida04@outlook.com>
2024-02-17 00:52:11 +0000

    Addition of plus, minus, calculate, delete and clear buttons + logic of plus, minus and calculate working

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,4 +1,5 @@
 const numbers = [];
+let result;
 
 function writeNumber(number){
     if(numbers.length <= 1){
@@ -6,9 +7,46 @@ function writeNumber(number){
         numbers[0] = number;
     }
     else{
-        document.getElementById("result").innerHTML = document.getElementById("result").innerHTML + number;
+        document.getElementById("result").innerHTML += number;
         numbers[2] = number;
     }
-    console.log(number);
+}
+
+
+function operation(option){
+    if(numbers.length == 0) return;
+    else if(option == 0){
+        numbers[1] = 0;
+        document.getElementById("result").innerHTML += "+";
+    }
+    else{
+        numbers[1] = 1;
+        document.getElementById("result").innerHTML += "-";
+    }
+}
+
+function clear(option){
+    console.log("teste1");
+    if(numbers.length == 0) return;
+    else if(option == 0){ 
+        numbers.pop();
+        for(let i = 0; i < numbers.length; i++){
+            let str = document.getElementById("result").innerHTML; 
+            document.getElementById("result").innerHTML = str.substring(0, str.length - 1);
+        }
+    }
+    else{
+        while(numbers.length != 0) numbers.pop();
+        document.getElementbyId("result").innerHTML = "";
+    } 
     console.log(numbers);
+}
+
+function calculate(){
+    if(numbers.length != 3) return;
+    else if(numbers[1] == 0)
+        result = numbers[0] + numbers[2];
+    else
+        result = numbers[0] - numbers[2];
+    document.getElementById("result").innerHTML = result; 
 }
