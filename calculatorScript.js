1f0d3d68d7e686074537e7076245d992c2f4f80c
BIRB <tiagojralmeida04@outlook.com>
2024-02-18 16:29:14 +0000

    Change in the javascript logic + calculator fully working

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,49 +1,69 @@
-const numbers = [];
-let result;
+let str;
+let currentNumber = "";
+let result = 0;
+let currentOperation = -1;
 
 function writeNumber(number){
-    if(numbers.length <= 1){
-        document.getElementById("result").innerHTML = number;
-        numbers[0] = number;
-    }
-    else{
-        document.getElementById("result").innerHTML += number;
-        numbers[2] = number;
-    }
+    document.getElementById("result").innerHTML += number; 
 }
 
 
 function operation(option){
-    if(numbers.length == 0) return;
-    else if(option == 0){
-        numbers[1] = 0;
+    str = document.getElementById("result").innerHTML;
+    if(str == "" || str.charAt(str.length - 1) == "+" || str.charAt(str.length - 1) == "-") 
+        return;
+    else if(option == 0) 
         document.getElementById("result").innerHTML += "+";
-    }
-    else{
-        numbers[1] = 1;
+    else if(option == 1)
         document.getElementById("result").innerHTML += "-";
-    }
+    else console.log("Error");
 }
 
 function clearResult(option){
-    if(numbers.length == 0) return;
-    else if(option == 0){ // Option 0 => Delete
-        numbers.pop();
-        let str = document.getElementById("result").innerHTML; 
-        document.getElementById("result").innerHTML = str.substring(0, str.length - 1);   
-    }
-    else{
-        while(numbers.length != 0) numbers.pop();
-        document.getElementById("result").innerHTML = "";
+    if(document.getElementById("result").innerHTML == "") console.log("empty");
+    else if(option == 0){
+        str = document.getElementById("result").innerHTML;
+        document.getElementById("result").innerHTML = str.substring(0, str.length - 1);
     } 
-    console.log(numbers);
+    else document.getElementById("result").innerHTML = "";
 }
 
-function calculate(){
-    if(numbers.length != 3) return;
-    else if(numbers[1] == 0)
-        result = numbers[0] + numbers[2];
-    else
-        result = numbers[0] - numbers[2];
-    document.getElementById("result").innerHTML = result; 
+
+function calculate(str){
+    str += "+";
+    for(let i = 0; i < str.length; i++){
+        if(str.charAt(i) == "+" || str.charAt(i) == "-"){
+            switch(currentOperation){
+                case 0:
+                    result += parseInt(currentNumber);
+                    break;
+                case 1:
+                    result -= parseInt(currentNumber);
+                    break;
+                default:
+                    result = parseInt(currentNumber);
+            }
+            currentOperation = str.charAt(i) === "+" ? 0 : 1;
+            currentNumber = ""; 
+        }
+        else{
+            currentNumber += str.charAt(i);
+        }
+    }
+    currentOperation = -1;
+    return result;
+}
+
+
+function showResult(){
+    str = document.getElementById("result").innerHTML;
+    // If there are no operating signs, doesnt do anything
+    if(str.search("\\+") == -1 && str.search("-") == -1) return;
+    // If the last input was a operating sign, alerts an error
+    else if (str.charAt(str.length - 1) == "+" || str.charAt(str.length - 1) == "-") 
+        alert("Invalid input");
+    else{
+        calculate(str);
+        document.getElementById("result").innerHTML = result;
+    }
 }
